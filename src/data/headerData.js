67c769f30d2882895e01ec8ad90559060beccd76
unknown <ash.990.007@gmail.com>
2022-11-04 19:46:33 +0530

    Image working

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,6 +6,6 @@ export const headerData = {
   desciption:
     "Strong in logics and designing with intuitive problem-solving skills. Passionate about implementing and learning new projects. Always ready to learn and implement new technologies to scale up my profile as well as knowledge and get new opportunities.",
   image:
-    "https://photos.google.com/share/AF1QipOmxL3k-qOFhS33-sP6DSQE1pws2vP9hBF7kmvYoclYzPqJ1kR2KnTlQNKga8_XnQ/photo/AF1QipMWr6EXbADcSNY5ThfiiOg-0lDgPjh-T2avnFEx?key=V3JUXzJLNUpRejNlVWpBM2xxQ2p2TzlUdkNCWG9n",
+    "https://raw.githubusercontent.com/Thynameisash/ProFolio/master/public/pic.jpg",
   resumePdf: resume,
 };
